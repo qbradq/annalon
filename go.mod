ea301e872eba06ef75cf00fbb82f70d4ed3e9ede
Norman B. Lancaster <qbradq@gmail.com>
2026-01-02 12:59:31 -0600

    feat: introduce Ebiten game engine with basic 'Hello World' application and VS Code launch configuration

--- go.mod
+++ go.mod
@@ -3,14 +3,21 @@ module github.com/qbradq/annalon
 go 1.25.5
 
 require (
+	github.com/ebitengine/gomobile v0.0.0-20250923094054-ea854a63cce1 // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.9.0 // indirect
 	github.com/go-gl/mathgl v1.2.0 // indirect
+	github.com/hajimehoshi/ebiten/v2 v2.9.7 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
 	github.com/qbradq/q2d v0.0.0-20251129230231-f407b354fc1d // indirect
 	github.com/qbradq/q3d v0.0.0-20251208110450-913bd2d9a85b // indirect
 	golang.org/x/image v0.33.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
 )
 
 replace (
-	github.com/qbradq/q2d v0.0.0-20251129230231-f407b354fc1d => lib/q2d v0.0.0-20251129230231-f407b354fc1d
-	github.com/qbradq/q3d v0.0.0-20251208110450-913bd2d9a85b => lib/q3d v0.0.0-20251208110450-913bd2d9a85b
+	github.com/qbradq/q2d => ./lib/q2d
+	github.com/qbradq/q3d => ./lib/q3d
 )
--- internal/annalon/main.go
+++ internal/annalon/main.go
@@ -1,5 +1,61 @@
 package annalon
 
-func Main() {
+import (
+	"math"
 
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+const (
+	LogicalWidth  = 640
+	LogicalHeight = 360
+)
+
+type Game struct {
+	Scale int
+}
+
+func (g *Game) Update() error {
+	// Handle Scale Down (F9)
+	if inpututil.IsKeyJustPressed(ebiten.KeyF9) {
+		g.Scale = int(math.Max(1, float64(g.Scale)/2))
+		ebiten.SetWindowSize(LogicalWidth*g.Scale, LogicalHeight*g.Scale)
+	}
+
+	// Handle Scale Up (F11)
+	if inpututil.IsKeyJustPressed(ebiten.KeyF11) {
+		g.Scale = int(math.Min(16, float64(g.Scale)*2))
+		ebiten.SetWindowSize(LogicalWidth*g.Scale, LogicalHeight*g.Scale)
+	}
+
+	// Handle Fullscreen Toggle (F10)
+	if inpututil.IsKeyJustPressed(ebiten.KeyF10) {
+		ebiten.SetFullscreen(!ebiten.IsFullscreen())
+	}
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello World")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return LogicalWidth, LogicalHeight
+}
+
+func Main() {
+	game := &Game{
+		Scale: 2,
+	}
+
+	ebiten.SetWindowSize(LogicalWidth*game.Scale, LogicalHeight*game.Scale)
+	ebiten.SetWindowTitle("Annalon")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+	if err := ebiten.RunGame(game); err != nil {
+		panic(err)
+	}
 }
